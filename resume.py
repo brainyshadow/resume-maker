33b00b63ccd9202a39de2d65f6542bb93bb0a9c0
seb997a <seb997a@hotmail.com>
2022-02-12 18:26:26 -0500

    Changed PDF library
    Changed from FPDF to pdfkit to allow for HTML templates.

--- resume.py
+++ resume.py
@@ -1,24 +1,5 @@
-from pickle import TRUE
-from textwrap import fill
-from fpdf import FPDF
-
-# save FPDF() class into
-# a variable pdf
-pdf = FPDF()
+import pdfkit
 
 fileName = "Resume"
 
-# Creates the first page 
-pdf.add_page()
-  
-# Sets the font property
-pdf.set_font("Arial", size = 15)
-  
-pdf.cell(200, 30, txt = "Sample Title", 
-         ln = 1, align = 'C', fill=TRUE)
-  
-pdf.cell(200, 10, txt = "Heading",
-         ln = 2, align = 'C', fill=TRUE)
-  
-# save the pdf with name .pdf
-pdf.output(fileName+".pdf")
+pdfkit.from_url('http://google.com', fileName+'.pdf')
