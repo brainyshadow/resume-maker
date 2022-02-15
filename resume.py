c4c2bc0709bf25e76c847ba1d95c95172a6b0307
seb997a <seb997a@hotmail.com>
2022-02-15 18:54:00 -0500

    More editable attributes
    Added name to the editable attributes and made other minor styling changes.

--- resume.py
+++ resume.py
@@ -17,18 +17,19 @@ html = open(
 # Parse HTML file in Beautiful Soup
 soup = bs(html, 'html.parser')
 
-# Give location where text is
-# stored which you wish to alter
-old_text = soup.find("p", {"id": "qulificationSummary"})
-print(old_text)
+name = "Joe Smith"
 
-# Replace the already stored text with
-# the new text which you wish to assign
-# Replace the already stored text with
-# the new text which you wish to assign
+# Replace Name
+old_text = soup.find("h2", {"id": "name"})
+new_text = old_text.find(text=re.compile('NAME')).replace_with(name.upper())
+
+
+# Replace Qualifications
+old_text = soup.find("p", {"id": "qulificationSummary"})
 new_text = old_text.find(
-    text=re.compile('This is where a summary of qualifications will go.'
-                    )).replace_with('This is the replacted list of some of my qualifications.')
+    text=re.compile('This is where a summary of qualifications will go.')
+).replace_with('This is the replacted list of some of my qualifications.')
+
 
 # Alter HTML file to see the changes done
 with open("temp.html", "wb") as f_output:
@@ -37,7 +38,7 @@ with open("temp.html", "wb") as f_output:
 fileName = "Resume"
 
 try:
-    pdfkit.from_file('./templates/template' + str(templateNumber) + '.html', fileName + '.pdf')
+    pdfkit.from_file('temp.html', fileName + '.pdf')
 
 except:
     print("An error occured")
