170e142652bdcce7636e3b06fe899bc00a3013f9
seb997a <seb997a@hotmail.com>
2022-03-07 16:42:51 -0500

    Made minor UI changes.

--- website/resume-maker/src/App.js
+++ website/resume-maker/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Generateresume from "./pages/Generateresume";
 import Home from "./pages/Home";
+import Upload from "./pages/Upload";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="*" element={<Navigate replace to="/home" />} />
         <Route path="home" element={<Home />} />
         <Route path="resume" element={<Generateresume />} />
+        <Route path="upload" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   );
