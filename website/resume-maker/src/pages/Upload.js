3a1074c7b3c4968f0852a6e98d913a7c37fa6822
BrainyShadow <seb997a@hotmail.com>
2022-03-17 17:36:02 -0400

    Started upload page
    Started to create the upload page for uploading a template. Intentionally made it form instead of a file input that way the HTML can be reviewed.

--- website/resume-maker/src/pages/Upload.js
+++ website/resume-maker/src/pages/Upload.js
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "../App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Upload extends Component {
+  async submitted() {}
   render() {
-    return <></>;
+    return (
+      <>
+        <Header />
+        <div className="main-container">
+          <Form onSubmit={this.submitted}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Template Name:</Form.Label>
+              <Form.Control required={true} placeholder="Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="qualifications">
+              <Form.Label>Template Description:</Form.Label>
+              <Form.Control
+                required={true}
+                as="textarea"
+                rows={3}
+                placeholder="Description"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Your Email:</Form.Label>
+              <Form.Control required={true} placeholder="Email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="qualifications">
+              <Form.Label>Template:</Form.Label>
+              <Form.Control
+                required={true}
+                as="textarea"
+                rows={15}
+                placeholder="HTML"
+              />
+            </Form.Group>
+            <Button type="submit" style={{ marginBottom: "1rem" }}>
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <Footer />
+      </>
+    );
   }
 }
 
