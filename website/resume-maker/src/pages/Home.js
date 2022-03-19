c5d26299fd569a9244474bb140cdf1f7ace077bb
BrainyShadow <seb997a@hotmail.com>
2022-03-18 22:03:01 -0400

    Updated home page.
    Made significant changes to the home page of the website.

--- website/resume-maker/src/pages/Home.js
+++ website/resume-maker/src/pages/Home.js
@@ -18,33 +18,57 @@ class Home extends Component {
   render() {
     return (
       <>
-        <div>
-          <Header />
-
-          <div className="main-container">
-            <div className="left-part">
-              <div className="only-vertical-center">
-                <Typography fontSize={"3rem"} variant="h1" marginLeft={"8rem"}>
-                  Build your resume.
-                </Typography>
-                <Typography variant="body" marginLeft={"8rem"}>
-                  Simple. No Acounts. Open Source.
-                </Typography>
-                <br></br>
-                <Typography variant="body" marginLeft={"8rem"}>
-                  <a href="/resume">Build now.</a>
-                </Typography>
+        <Header />
+        <div className="main-container-min-height">
+          <div className="left-part">
+            <div className="only-vertical-center">
+              <Typography fontSize={"3rem"} variant="h1" marginLeft={"8rem"}>
+                Build your resume.
+              </Typography>
+              <Typography variant="body" marginLeft={"8rem"}>
+                Simple. No Acounts. Open Source.
+              </Typography>
+              <br></br>
+              <Typography variant="body" marginLeft={"8rem"}>
+                <a href="/resume">Build now.</a>
+              </Typography>
+            </div>
+          </div>
+          <div className="right-part">
+            <div className="only-vertical-center">
+              <div className="yellow-accent">
+                <div className="vertical-center">
+                  <Document file={TemplateOne}>
+                    <Page scale={0.7} pageNumber={1} />
+                  </Document>
+                </div>
               </div>
             </div>
-            <div className="right-part">
+          </div>
+        </div>
+        <div style={{ backgroundColor: "#F0F0F0" }}>
+          <div
+            className="main-container"
+            style={{ backgroundColor: "#F0F0F0" }}
+          >
+            <div className="small-left-part">
               <div className="only-vertical-center">
-                <div className="yellow-accent">
-                  <div className="vertical-center">
-                    <Document file={TemplateOne}>
-                      <Page scale={0.7} pageNumber={1} />
-                    </Document>
-                  </div>
+                <Typography fontSize={"3rem"} variant="h1" marginLeft={"8rem"}>
+                  Fill out our form.
+                </Typography>
+                <div style={{ marginLeft: "8rem" }}>
+                  <Typography variant="body">
+                    Fill out our form. This gives the generator your
+                    qualifications allowing it to create a personalized resume
+                    for you.
+                  </Typography>
                 </div>
+                <br></br>
+              </div>
+            </div>
+            <div className="large-right-part">
+              <div className="vertical-center">
+                <ProgressiveImage src={Form}></ProgressiveImage>
               </div>
             </div>
           </div>
@@ -53,44 +77,33 @@ class Home extends Component {
               className="main-container"
               style={{ backgroundColor: "#F0F0F0" }}
             >
-              <div style={{ height: "2rem" }}> </div>
-              <Typography
-                fontSize={"3rem"}
-                variant="h1"
-                marginX={"8rem"}
-                marginY="2rem"
-              >
-                How it works.
-              </Typography>
-              <div>
-                <Typography
-                  fontSize={"1rem"}
-                  variant="body"
-                  position="relative"
-                  marginY={"1rem"}
-
-                >
-                  Fill out our form. This gives the generator your
-                  qualifications allowing it to create a personalized resume for
-                  you.
-                </Typography>
-                <ProgressiveImage src={Form} ></ProgressiveImage>
-                <Typography
-                  fontSize={"1rem"}
-                  variant="body"
-                  position="relative"
-                  marginY={"1rem"}
-                >
-                  Select a template. This tells us what you want your custom
-                  resume to look like. From here, we can fill our templates in
-                  with the qualifications <b>you</b> gave us.
-                </Typography>
-                <ProgressiveImage src={Select} className="center-main-image"></ProgressiveImage>
+              <div className="small-left-part">
+                <div className="vertical-center">
+                  <Typography
+                    fontSize={"3rem"}
+                    variant="h1"
+                    marginLeft={"8rem"}
+                  >
+                    Pick a template.
+                  </Typography>
+                  <div style={{ marginLeft: "8rem" }}>
+                    <Typography variant="body">
+                      Select a template. This tells us what you want your custom
+                      resume to look like. From here, we can fill our templates
+                      in with the qualifications you gave us.
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="vertical-center">
+                <div className="large-right-part">
+                  <ProgressiveImage src={Select}></ProgressiveImage>
+                </div>
               </div>
             </div>
           </div>
-          <Footer />
         </div>
+        <Footer />
       </>
     );
   }
