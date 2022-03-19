31d6fc2ac6360eaea9645e2cfd919a182c126213
BrainyShadow <seb997a@hotmail.com>
2022-03-18 22:15:21 -0400

    Update upload template
    Updated the upload template process. Updated the upload template process to make uploading templates more user friendly.

--- website/resume-maker/src/pages/Upload.js
+++ website/resume-maker/src/pages/Upload.js
@@ -7,43 +7,56 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Upload extends Component {
-  async submitted() {}
+  async handleSubmit(e) {
+    e.preventDefault();
+    let userInput = e.target.elements;
+
+    var data = {
+      TemplateName: userInput.templateName.value,
+      TemplateDescription: userInput.templateDescription.value,
+      Email: userInput.email.value,
+      Html: userInput.html.value,
+    };
+  }
+
   render() {
     return (
       <>
         <Header />
-        <div className="main-container">
-          <Form onSubmit={this.submitted}>
-            <Form.Group className="mb-3" controlId="name">
-              <Form.Label>Template Name:</Form.Label>
-              <Form.Control required={true} placeholder="Name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="qualifications">
-              <Form.Label>Template Description:</Form.Label>
-              <Form.Control
-                required={true}
-                as="textarea"
-                rows={3}
-                placeholder="Description"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="name">
-              <Form.Label>Your Email:</Form.Label>
-              <Form.Control required={true} placeholder="Email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="qualifications">
-              <Form.Label>Template:</Form.Label>
-              <Form.Control
-                required={true}
-                as="textarea"
-                rows={15}
-                placeholder="HTML"
-              />
-            </Form.Group>
-            <Button type="submit" style={{ marginBottom: "1rem" }}>
-              Submit
-            </Button>
-          </Form>
+        <div style={{ alignItems: "center" }}>
+          <div className="form-container">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group className="mb-3" controlId="templateName">
+                <Form.Label>Template Name:</Form.Label>
+                <Form.Control required={true} placeholder="Name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="templateDescription">
+                <Form.Label>Template Description:</Form.Label>
+                <Form.Control
+                  required={true}
+                  as="textarea"
+                  rows={3}
+                  placeholder="Description"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Your Email:</Form.Label>
+                <Form.Control required={true} placeholder="Email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="html">
+                <Form.Label>Template:</Form.Label>
+                <Form.Control
+                  required={true}
+                  as="textarea"
+                  rows={15}
+                  placeholder="HTML"
+                />
+              </Form.Group>
+              <Button type="submit" style={{ marginBottom: "1rem" }}>
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
         <Footer />
       </>
