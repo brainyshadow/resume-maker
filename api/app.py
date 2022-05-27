af680bac43615e27400d57bc041ff53301a8300f
BrainyShadow <seb997a@hotmail.com>
2022-05-27 16:47:18 -0400

    Added route to API
    Added a route to the API to just return a resume's html that will then be converted to a pdf on the front end.

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ import asyncio
 from jinja2 import TemplateSyntaxError
 from sqlalchemy import false, true
 import json
-from resume import generateResume
+from resume import generateResume, generateHTMl
 from confirmation_email import sendConfirmationEmail
 import os
 from decouple import config
@@ -105,6 +105,7 @@ def downloadFile():
             asyncio.set_event_loop(asyncio.new_event_loop())
             loop = asyncio.get_event_loop()
             loop.run_until_complete(makeResume(HTML, content))
+            generateHTMl(HTML, content)
             loop.stop()
             path = "./Resume.pdf"
             try:
@@ -112,6 +113,53 @@ def downloadFile():
             finally:
                 os.remove("Resume.pdf")
             # route for adding custom template
+        else:
+            return "Invalid Token", 401
+    else:
+        return "Invalid Template Id", 400
+
+
+@app.route('/downloadhtml', methods=['POST'])
+@cross_origin()
+def downloadhtml():
+    content = request.data
+    parsedRequestBody = json.loads(content)
+    templateId = int(parsedRequestBody["Template Id"])
+
+    cursor = templateCollection.find({"TemplateID": templateId}).limit(1)
+    list_cur = list(cursor)
+    template = list_cur[0]
+    aprovalStatus = template["Approved"]
+    if(aprovalStatus):
+        HTML = template["HTML"]
+        downloadCount = template["DownloadCount"]
+        documentId = template["_id"]
+        templateCollection.update_one(
+            {"_id": documentId}, {"$set": {"DownloadCount": downloadCount+1}})
+
+        recaptchaKey = config('recaptchaKey')
+        recaptchaAPIkey = config('API_KEY')
+        projectId = config('projectId')
+
+        token = request.headers.get('reCAPTCHA-Token')
+        exceptedAction = "generateResume"
+        response = requests.post(
+            "https://recaptchaenterprise.googleapis.com/v1beta1/projects/"+projectId+"/assessments?key="+recaptchaAPIkey, json={
+                "event": {
+                    "token": token,
+                    "siteKey": recaptchaKey,
+                    "expectedAction": exceptedAction
+                }
+            }
+        )
+        score = response.json()["score"]
+        action = response.json()["tokenProperties"]["action"]
+        if(score > 0.5 and action == exceptedAction):
+            # the resume function is async and must be waited for
+
+            html = generateHTMl(HTML, content)
+            return html
+
         else:
             return "Invalid Token", 401
     else:
@@ -155,10 +203,11 @@ def uploadTemplate():
         HTML = jsonData["HTML"]
         userEmail = jsonData["Email"]
         templateCollection.insert_one({"TemplateID": newTemplateId, "TempalteName": tempalteName,
-                                     "TemplateDescription": tempalteDescription, "HTML": HTML, "Approved": False, "DownloadCount": 0, "UserEmail": userEmail})
+                                       "TemplateDescription": tempalteDescription, "HTML": HTML, "Approved": False, "DownloadCount": 0, "UserEmail": userEmail})
         email = config('email')
         password = config('password')
-        sendConfirmationEmail(email, password, userEmail, tempalteName, tempalteDescription, HTML )
+        sendConfirmationEmail(email, password, userEmail,
+                              tempalteName, tempalteDescription, HTML)
         print("Valid")
         return "", 200
     else:
